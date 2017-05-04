cca8fb73fb774bda50a4312c3cab5bce1556b2b8
Elad Plotski <elad.p@onoapps.com>
2017-05-04 16:22:34 +0300

    no message

--- services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationService.java
+++ services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationService.java
@@ -13,18 +13,13 @@ import com.kaltura.netkit.services.api.ott.phoenix.PhoenixRequestBuilder;
 
 public class OttPushNotificationService extends PhoenixService {
 
-    public static PhoenixRequestBuilder userLogin(String baseUrl, int partnerId, String username, String password){
-        return userLogin(baseUrl, partnerId, username, password, null);
+    public static PhoenixRequestBuilder setDevicePushToken(String baseUrl, String token){
+        return setDevicePushToken(baseUrl, token);
     }
 
-    public static PhoenixRequestBuilder userLogin(String baseUrl, int partnerId, String username, String password, @Nullable String udid){
+    public static PhoenixRequestBuilder setDevicePushToken(String baseUrl, String token){
         JsonObject params = new JsonObject();
-        params.addProperty("partnerId", partnerId);
-        params.addProperty("username", username);
-        params.addProperty("password", password);
-        if(udid != null){
-            params.addProperty("udid", udid);
-        }
+        params.addProperty("token", token);
 
         return new PhoenixRequestBuilder()
                 .service("ottUser")
@@ -33,82 +28,5 @@ public class OttPushNotificationService extends PhoenixService {
                 .url(baseUrl)
                 .tag("ottuser-login")
                 .params(params);
-    }
-
-    public static PhoenixRequestBuilder anonymousLogin(String baseUrl, int partnerId, @Nullable String udid){
-        JsonObject params = new JsonObject();
-        params.addProperty("partnerId", partnerId);
-        if(udid != null){
-            params.addProperty("udid", udid);
-        }
-
-        return new PhoenixRequestBuilder()
-                .service("ottUser")
-                .action("anonymousLogin")
-                .method("POST")
-                .url(baseUrl)
-                .tag("ottuser-anonymous-login")
-                .params(params);
-    }
-
-    public static PhoenixRequestBuilder refreshSession(String baseUrl, String ks, String refreshToken, @Nullable String udid){
-        JsonObject params = new JsonObject();
-        params.addProperty("ks", ks);
-        params.addProperty("refreshToken", refreshToken);
-        if(!TextUtils.isEmpty(udid)) {
-            params.addProperty("udid", udid);
-        }
-
-        return new PhoenixRequestBuilder()
-                .service("ottUser")
-                .action("refreshSession")
-                .method("POST")
-                .url(baseUrl)
-                .tag("ottuser-refresh-session")
-                .params(params);
-    }
-
-    public static PhoenixRequestBuilder logout(String baseUrl, String ks, @Nullable String udid){//?? check if udid needed here
-        JsonObject params = new JsonObject();
-        params.addProperty("ks", ks);
-        if(!TextUtils.isEmpty(udid)) {
-            params.addProperty("udid", udid);
-        }
-
-        return new PhoenixRequestBuilder()
-                .service("ottUser")
-                .action("logout")
-                .method("POST")
-                .url(baseUrl)
-                .tag("ottuser-logout")
-                .params(params);
-    }
-
-    public static PhoenixRequestBuilder socialLogin(@NonNull String baseUrl, int partnerId, @NonNull String token, String socialNetwork, @Nullable String udid) {
-        JsonObject params = new JsonObject();
-        params.addProperty("token", token);
-        params.addProperty("partnerId", partnerId);
-        params.addProperty("type", socialNetwork);
-        if(udid != null) {
-            params.addProperty("udid", udid);
-        }
-
-        return new PhoenixRequestBuilder()
-                .service("social")
-                .action("login")
-                .method("POST")
-                .url(baseUrl)
-                .tag("social-login")
-                .params(params);
-    }
-
-    public enum KalturaSocialNetwork{
-        FACEBOOK("facebook");
-
-        public String value;
-
-        KalturaSocialNetwork(String value){
-            this.value = value;
-        }
     }
 }
