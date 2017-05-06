4a3764643593f1dfd2cca4cb987184b8d63fcb40
Elad Plotski <elad.p@onoapps.com>
2017-05-06 14:58:11 +0300

    Fix

--- services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationService.java
+++ services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationService.java
@@ -13,23 +13,22 @@ import com.kaltura.netkit.services.api.ott.phoenix.PhoenixRequestBuilder;
 
 public class OttPushNotificationService extends PhoenixService {
 
-//    public static String setDevicePushToken(String baseUrl, String token){
-//        //return setDevicePushToken(baseUrl, token);
-//        return "";//new PhoenixRequestBuilder();
-//    }
-//
-//    public static String setDevicePushToken(String baseUrl, String token){
-////        JsonObject params = new JsonObject();
-////        params.addProperty("token", token);
-////
-////        return new PhoenixRequestBuilder()
-////                .service("ottUser")
-////                .action("login")
-////                .method("POST")
-////                .url(baseUrl)
-////                .tag("ottuser-login")
-////                .params(params);
-//
-//       return "";
-//    }
+    public static PhoenixRequestBuilder setDevicePushToken(String baseUrl, String token){
+        return setDevicePushToken(baseUrl, token);
+    }
+
+
+    public static PhoenixRequestBuilder setDevicePushToken(String baseUrl, String token){
+        JsonObject params = new JsonObject();
+        params.addProperty("token", token);
+
+        return new PhoenixRequestBuilder()
+                .service("ottPush")
+                .action("register")
+                .method("POST")
+                .url(baseUrl)
+                .tag("ottuser-register")
+                .params(params);
+
+    }
 }
