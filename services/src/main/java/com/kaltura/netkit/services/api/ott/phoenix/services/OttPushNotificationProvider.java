08911fd385d8b35c151763fa6a3c4b1b23f83499
Elad Plotski <elad.p@onoapps.com>
2017-05-11 23:21:00 +0300

    no message

--- services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationProvider.java
+++ services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/OttPushNotificationProvider.java
@@ -11,6 +11,7 @@ import com.kaltura.netkit.services.api.ott.phoenix.session.OttSessionProvider;
 import com.kaltura.netkit.utils.ErrorElement;
 import com.kaltura.netkit.utils.OnCompletion;
 import com.kaltura.netkit.utils.OnRequestCompletion;
+import com.kaltura.netkit.utils.SessionProvider;
 
 /**
  * Created by eladplotski on 08/05/2017.
@@ -20,119 +21,149 @@ public class OttPushNotificationProvider {
 
     private static final String TAG = "OttPushProvider";
     private static String apiBaseUrl = "";
-    private static String ks = "";
 
-    public OttPushNotificationProvider(String baseUrl, String ks) {
+    public OttPushNotificationProvider(String baseUrl) {
         this.apiBaseUrl = baseUrl;
-        this.ks = ks;
     }
 
     /*Push Notification Section*/
 
-    public void setDevicePushToken(final String pushToken){
+    public void setDevicePushToken(final String pushToken, SessionProvider sp){
 
-        APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.setDevicePushToken(apiBaseUrl, pushToken,ks)
-                .completion(new OnRequestCompletion() {
-                    @Override
-                    public void onComplete(ResponseElement response) {
+        sp.getSessionToken(new OnCompletion<PrimitiveResult>() {
+            @Override
+            public void onComplete(PrimitiveResult response) {
 
-                        ErrorElement error = null;
-                        if (response != null && response.isSuccess()) {
-                            Log.d(TAG, "push token registartion : Succsess.");
-                        } else {
-                            error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token Registration Failed");
-                            Log.e(TAG, "push token reßgistartion : Failed with error - " + error.getMessage());
-                        }
+                String ks = response.getResult();
 
-                    }
-                }).build());
+                APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.setDevicePushToken(apiBaseUrl, pushToken,ks)
+                        .completion(new OnRequestCompletion() {
+                            @Override
+                            public void onComplete(ResponseElement response) {
+
+                                ErrorElement error = null;
+                                if (response != null && response.isSuccess()) {
+                                    Log.d(TAG, "push token registartion : Succsess.");
+                                } else {
+                                    error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token Registration Failed");
+                                    Log.e(TAG, "push token reßgistartion : Failed with error - " + error.getMessage());
+                                }
+
+                            }
+                        }).build());
+            }
+        });
     }
 
-    public void addFollowTVSeries(int mediaid,final OnCompletion<ResponseElement> pushStateCallback){
+    public void addFollowTVSeries(final int mediaid, SessionProvider sp,final OnCompletion<ResponseElement> pushStateCallback){
 
-        APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.addFollowTVSeries(apiBaseUrl,ks,mediaid)
-                .completion(new OnRequestCompletion() {
-                    @Override
-                    public void onComplete(ResponseElement response) {
+        sp.getSessionToken(new OnCompletion<PrimitiveResult>() {
+            @Override
+            public void onComplete(PrimitiveResult response) {
 
-                        ErrorElement error = null;
-                        if (response != null && response.isSuccess()) {
-                            Log.d(TAG, "addFollowTVSeries : Succsess.");
-                            pushStateCallback.onComplete(response);
-                        } else {
-                            error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
-                            pushStateCallback.onComplete(response);
-                            Log.e(TAG, "addFollowTVSeries : Failed with error - " + error.getMessage());
-                        }
+                String ks = response.getResult();
 
-                    }
-                }).build());
+                APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.addFollowTVSeries(apiBaseUrl,ks,mediaid)
+                        .completion(new OnRequestCompletion() {
+                            @Override
+                            public void onComplete(ResponseElement response) {
 
+                                ErrorElement error = null;
+                                if (response != null && response.isSuccess()) {
+                                    Log.d(TAG, "addFollowTVSeries : Succsess.");
+                                    pushStateCallback.onComplete(response);
+                                } else {
+                                    error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
+                                    pushStateCallback.onComplete(response);
+                                    Log.e(TAG, "addFollowTVSeries : Failed with error - " + error.getMessage());
+                                }
+
+                            }
+                        }).build());
+            }});
     }
 
-    public void deleteFollowTVSeries(int mediaid,final OnCompletion<ResponseElement> pushStateCallback){
+    public void deleteFollowTVSeries(final int mediaid, SessionProvider sp,final OnCompletion<ResponseElement> pushStateCallback){
 
-        APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.deleteFollowTVSeries(apiBaseUrl,ks,mediaid)
-                .completion(new OnRequestCompletion() {
-                    @Override
-                    public void onComplete(ResponseElement response) {
+        sp.getSessionToken(new OnCompletion<PrimitiveResult>() {
+            @Override
+            public void onComplete(PrimitiveResult response) {
 
-                        ErrorElement error = null;
-                        if (response != null && response.isSuccess()) {
-                            Log.d(TAG, "addFollowTVSeries : Succsess.");
-                            pushStateCallback.onComplete(response);
-                        } else {
-                            error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
-                            pushStateCallback.onComplete(response);
-                            Log.e(TAG, "addFollowTVSeries : Failed with error - " + error.getMessage());
-                        }
+                String ks = response.getResult();
 
-                    }
-                }).build());
+                APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.deleteFollowTVSeries(apiBaseUrl,ks,mediaid)
+                        .completion(new OnRequestCompletion() {
+                            @Override
+                            public void onComplete(ResponseElement response) {
 
+                                ErrorElement error = null;
+                                if (response != null && response.isSuccess()) {
+                                    Log.d(TAG, "addFollowTVSeries : Succsess.");
+                                    pushStateCallback.onComplete(response);
+                                } else {
+                                    error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
+                                    pushStateCallback.onComplete(response);
+                                    Log.e(TAG, "addFollowTVSeries : Failed with error - " + error.getMessage());
+                                }
+
+                            }
+                        }).build());
+            }});
     }
 
-    public void getPushNotificationStates(final OnCompletion<ResponseElement> pushStateCallback){
+    public void getPushNotificationStates(SessionProvider sp,final OnCompletion<ResponseElement> pushStateCallback){
 
-        APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.getNotificationSettingsStatus(apiBaseUrl,ks)
-                .completion(new OnRequestCompletion() {
-                    @Override
-                    public void onComplete(ResponseElement response) {
+        sp.getSessionToken(new OnCompletion<PrimitiveResult>() {
+            @Override
+            public void onComplete(PrimitiveResult response) {
 
-                        ErrorElement error = null;
-                        if (response != null && response.isSuccess()) {
-                            Log.d(TAG, "push states : Succsess.");
-                            pushStateCallback.onComplete(response);
-                        } else {
-                            error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
-                            pushStateCallback.onComplete(response);
-                            Log.e(TAG, "push states : Failed with error - " + error.getMessage());
-                        }
+                String ks = response.getResult();
 
-                    }
-                }).build());
+                APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.getNotificationSettingsStatus(apiBaseUrl,ks)
+                        .completion(new OnRequestCompletion() {
+                            @Override
+                            public void onComplete(ResponseElement response) {
 
+                                ErrorElement error = null;
+                                if (response != null && response.isSuccess()) {
+                                    Log.d(TAG, "push states : Succsess.");
+                                    pushStateCallback.onComplete(response);
+                                } else {
+                                    error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
+                                    pushStateCallback.onComplete(response);
+                                    Log.e(TAG, "push states : Failed with error - " + error.getMessage());
+                                }
+
+                            }
+                        }).build());
+            }});
     }
 
-    public void setPushNotificationStates(boolean allowNotification,boolean allowFollowNotification, final OnCompletion<ResponseElement> pushStateCallback){
+    public void setPushNotificationStates(final boolean allowNotification, final boolean allowFollowNotification, SessionProvider sp, final OnCompletion<ResponseElement> pushStateCallback){
 
-        APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.setNotificationSettingsStatus(apiBaseUrl,ks,allowNotification,allowFollowNotification)
-                .completion(new OnRequestCompletion() {
-                    @Override
-                    public void onComplete(ResponseElement response) {
+        sp.getSessionToken(new OnCompletion<PrimitiveResult>() {
+            @Override
+            public void onComplete(PrimitiveResult response) {
 
-                        ErrorElement error = null;
-                        if (response != null && response.isSuccess()) {
-                            Log.d(TAG, "push state update : Succsess.");
-                            pushStateCallback.onComplete(response);
-                        } else {
-                            pushStateCallback.onComplete(response);
-                            error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
-                            Log.e(TAG, "push state update : Failed with error - " + error.getMessage());
-                        }
+                String ks = response.getResult();
 
-                    }
-                }).build());
+                APIOkRequestsExecutor.getSingleton().queue(OttPushNotificationService.setNotificationSettingsStatus(apiBaseUrl,ks,allowNotification,allowFollowNotification)
+                        .completion(new OnRequestCompletion() {
+                            @Override
+                            public void onComplete(ResponseElement response) {
 
+                                ErrorElement error = null;
+                                if (response != null && response.isSuccess()) {
+                                    Log.d(TAG, "push state update : Succsess.");
+                                    pushStateCallback.onComplete(response);
+                                } else {
+                                    pushStateCallback.onComplete(response);
+                                    error = response.getError() != null ? response.getError() : ErrorElement.GeneralError.message("Push Token states Failed");
+                                    Log.e(TAG, "push state update : Failed with error - " + error.getMessage());
+                                }
+
+                            }
+                        }).build());
+            }});
     }
 }
