313722dd0e1f57efc2732793c2bac26e154b5330
tehilar <tehila.rozin@kaltura.com>
2017-05-28 17:52:00 +0300

    enable set configuration per request + add default configuration setter and logs enabling on RequestsExecutor (#1)

--- netkit/src/main/java/com/kaltura/netkit/connect/executor/RequestQueue.java
+++ netkit/src/main/java/com/kaltura/netkit/connect/executor/RequestQueue.java
@@ -1,10 +1,13 @@
 package com.kaltura.netkit.connect.executor;
 
 
+import com.kaltura.netkit.connect.request.RequestConfiguration;
 import com.kaltura.netkit.connect.request.RequestElement;
 import com.kaltura.netkit.connect.response.ResponseElement; /**
  */
 public interface RequestQueue {
+
+    void setDefaultConfiguration(RequestConfiguration config);
 
     String queue(RequestElement request);
 
@@ -17,4 +20,6 @@ public interface RequestQueue {
     void clearRequests();
 
     boolean isEmpty();
+
+    void enableLogs(boolean enable);
 }
--- netkit/src/main/java/com/kaltura/netkit/connect/request/RequestConfiguration.java
+++ netkit/src/main/java/com/kaltura/netkit/connect/request/RequestConfiguration.java
@@ -6,8 +6,10 @@ package com.kaltura.netkit.connect.request;
 public interface RequestConfiguration {
 
     long getReadTimeout();
+
     long getWriteTimeout();
+
     long getConnectTimeout();
+
     int getRetry();
-    boolean printLogs();
 }
