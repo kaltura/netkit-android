fbdc267ac1e1a1a3ed1260cbc025cce001dce731
zivilan <ziv.ilan@kaltura.com>
2017-09-03 14:36:01 +0300

    Time shifted service (#4)
    * added service timeShift
    
    * added property
    
    * fixes model
    
    * fixes model
    
    * fixes for git ignore
    
    * removing iml files
    
    * iml removal
    
    * removing another iml

---
+++ services/src/main/java/com/kaltura/netkit/services/api/ott/phoenix/services/TimeShiftedService.java
@@ -0,0 +1,29 @@
+package com.kaltura.netkit.services.api.ott.phoenix.services;
+
+import com.google.gson.JsonObject;
+import com.kaltura.netkit.services.api.ott.phoenix.PhoenixRequestBuilder;
+
+/**
+ * Created by zivilan on 31/07/2017.
+ */
+
+public class TimeShiftedService extends PhoenixService {
+
+    public static PhoenixRequestBuilder actionGet(String baseUrl, int partnerId, String ks) {
+        return new PhoenixRequestBuilder()
+                .service("timeshiftedtvpartnersettings")
+                .action("get")
+                .method("POST")
+                .url(baseUrl)
+                .tag("timeshifted-action-get")
+                .params(getTimeShiftedGetReqParams(ks));
+    }
+
+    private static JsonObject getTimeShiftedGetReqParams(String ks) {
+        JsonObject getParams = new JsonObject();
+        getParams.addProperty("ks", ks);
+        getParams.addProperty("objectType", "KalturaTimeShiftedTvPartnerSettings");
+
+        return getParams;
+    }
+}
