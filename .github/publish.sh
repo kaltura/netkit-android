8db7ef3c6333e9ab2311c751b120d902d9adde47
Gourav Saxena <38206744+GouravSna@users.noreply.github.com>
2023-01-05 13:09:35 +0530

    fix(FEC-12558): Generate RN by Github Actions (#46)

--- .github/publish.sh
+++ .github/publish.sh
@@ -1,19 +1,31 @@
-fail() {
+#!/bin/bash
+# This script takes care of the following tasks,
+## 1. Create a new branch from the default 'develop' or 'dev' branch.
+## 2. Change the version in version.gradle file.
+## 3. Commit and push the version.gradle changes to remote.
+## 4. Call `release_notes.sh` to generate release notes.
+## 5. Publish and Close it for SONATYPE Maven publishing.
+## 6. Create a TAG with release notes and push to remote.
+## 7. Notify MS-Teams when the whole process it done.
+## IMP: For Patch, there should always be a branch pushed to the remote
+## named as `patch/v*.*.*`. Because for patch, script is checking out that branch
+## and tagging it.
+
+exit_on_failure() {
     echo "$@" 1>&2
     exit 1
 }
 
 checkout() {
     echo Checking out newtag = "$NEW_TAG", release type = "$RELEASE_TYPE"
-    git fetch --all
 
     case $RELEASE_TYPE in
       Full)
-          git checkout -b "$BRANCH_NAME" || fail "Unable to checkout $BRANCH_NAME";;
+          git checkout -b "$BRANCH_NAME" || exit_on_failure "Unable to checkout $BRANCH_NAME";;
       Patch)
-          git checkout "$BRANCH_NAME" || fail "Unable to checkout $BRANCH_NAME";;
+          git checkout "$BRANCH_NAME" || exit_on_failure "Unable to checkout $BRANCH_NAME";;
       Update)
-          git checkout -b "$BRANCH_NAME" "$PREV_TAG" || fail "Unable to checkout $BRANCH_NAME";;
+          git checkout -b "$BRANCH_NAME" "$PREV_TAG" || exit_on_failure "Unable to checkout $BRANCH_NAME";;
     esac
 }
 
@@ -37,16 +49,23 @@ release_and_tag() {
     echo Releasing version $NEW_VERSION of $REPO_NAME to GitHub
     set +e
     git add $VERSION_FILE
+    git add $NETKIT_SERVICES_VERSION_FILE
     git commit -m "Update version to $NEW_TAG"
     set -e
     git push origin HEAD:$BRANCH_NAME
 
+    # Generate Release notes
+    bash $RELEASE_NOTES_SCRIPT
+
     if [[ "$RELEASE_TYPE" = "Patch" || "$RELEASE_TYPE" = "Full" ]]; then
+
+    releaseNotes=$(awk -v d="\\\n" '{s=(NR==1?s:s d)$0}END{print s}' $RELEASE_NOTES)
+
 
 cat << EOF > ./post.json
 {
       "name": "$NEW_TAG",
-      "body": "## Changes from [$PREV_TAG](https://github.com/kaltura/$REPO_NAME/releases/tag/$PREV_TAG)\n\nTBD",
+      "body": "$releaseNotes",
       "tag_name": "$NEW_TAG",
       "target_commitish": "$BRANCH_NAME"
 }
@@ -82,6 +101,7 @@ EOF
          -d@post.json
 
     rm ./post.json
+    rm $RELEASE_NOTES
 
     # delete temp branch
     #git push origin --delete $BRANCH_NAME
@@ -141,19 +161,19 @@ EOF
   GH_USER_NAME="Github Actions Bot KLTR"
   RELEASE_TYPE=$RELEASE_TYPE
 
-  REPO_NAME=$REPO_NAME
+  export REPO_NAME=$REPO_NAME
   MODULE_NAME=$MODULE_NAME
   VERSION_FILE=$MODULE_NAME/version.gradle
   NETKIT_SERVICES_VERSION_FILE="services/version.gradle" #Special variable only for netkit to change the version file version
 
   REPO_URL=https://github.com/kaltura/$REPO_NAME
-  NEW_VERSION=$NEW_VERSION
+  export NEW_VERSION=$NEW_VERSION
   PREV_VERSION=$PREV_VERSION
   TOKEN=$TOKEN
   TEAMS_WEBHOOK=$TEAMS_WEBHOOK
 
   NEW_TAG=v$NEW_VERSION
-  PREV_TAG=v$PREV_VERSION
+  export PREV_TAG=v$PREV_VERSION
   RELEASE_URL=$REPO_URL/releases/tag/$NEW_TAG
 
   if [[ "$RELEASE_TYPE" = "Full" || "$RELEASE_TYPE" = "Update" ]]; then
@@ -163,6 +183,9 @@ EOF
   if [ "$RELEASE_TYPE" = "Patch" ]; then
   BRANCH_NAME="patch/$NEW_TAG"
   fi
+
+  export RELEASE_NOTES="release_notes.md"
+  RELEASE_NOTES_SCRIPT=".github/release_notes.sh"
 
   checkout
   set_version
